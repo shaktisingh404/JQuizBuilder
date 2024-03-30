6f6a0aec902e5c76f43ff570bd8456090aca2bed
Shakti <2004shakti@gmail.com>
2024-03-30 11:29:11 +0530

    Phase 1 completed where(main,signup,login,mainpage)

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        login login = new login();
+        login.loginView();
+    }
+}
